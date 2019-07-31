504a50e7645e055dae2e389ae8e41193f9cfe4ef
naeem-akhtar <nakhtar.7777@gmail.com>
2019-08-01 00:17:32 +0530

    added frontend

---
+++ leadmanager/frontend/src/components/leads/Dashboard.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react';
+import Form from './Form';
+import Lead from './Leads';
+
+export default function Dashboard() {
+  return (
+    <Fragment>
+      <Form/>
+      <Lead/>
+    </Fragment>
+  )
+}
+
